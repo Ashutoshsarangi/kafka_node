789f2a339bf0e5b3ed65b54aa9efb25fb00db192
Ashu <ashutosh.softronet@gmail.com>
2019-12-02 18:33:02 +0530

    Done using kafka provider add link of usage of kafa in this app.js file

--- app.js
+++ app.js
@@ -11,11 +11,38 @@ app.use(function (req, res, next) {
 
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+var Kafka = require('no-kafka');
 const port = 3000
 
 io.on('connection', function (socket) {
   console.log('a user connected');
-  socket.emit("test event", { message: 'test Data 123' });
+  // socket.emit("test event", { message: 'test Data 123' });
 });
 
-http.listen(port, () => console.log(`Example app listening on port ${port}!`))
+http.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`)
+  var consumer = new Kafka.SimpleConsumer({
+    clientId: 'no-kafka-client',
+    connectionString: 'kafka://127.0.0.1:9092'
+  });
+
+  // data handler function can return a Promise 
+  var dataHandler = function (messageSet, topic, partition) {
+    messageSet.forEach(function (m) {
+      console.log(topic, partition, m.offset, m.message.value.toString('utf8'));
+      if (topic == "test") {
+        io.emit('test event', { message: m.message.value.toString('utf8') });
+      }
+    });
+  };
+  console.log(consumer);
+  return consumer.init().then(function () {
+    // Subscribe partitons 0 and 1 in a topic: 
+    var v1 = consumer.subscribe('test', dataHandler);
+    // var arr = [];
+    // arr.push([v1);
+    console.log("val:" + v1);
+    // return arr;
+
+  });
+});
