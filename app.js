a0db724668aa39fbc2fc17c965cb783e78ce797d
Ashu <ashutosh.softronet@gmail.com>
2019-12-10 12:38:41 +0530

    Final Code for commit

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ const port = 3000
 
 io.on('connection', function (socket) {
   console.log('a user connected');
-  // socket.emit("test event", { message: 'test Data 123' });
-});
+})
 
 http.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
@@ -26,24 +25,20 @@ http.listen(port, () => {
     clientId: 'no-kafka-client',
     connectionString: 'kafka://127.0.0.1:9092'
   });
-
   // data handler function can return a Promise 
   var dataHandler = function (messageSet, topic, partition) {
     messageSet.forEach(function (m) {
-      console.log(topic, partition, m.offset, m.message.value.toString('utf8'));
-      if (topic == "test") {
-        io.emit('test event', { message: m.message.value.toString('utf8') });
-      }
+      //console.log(topic, partition, m.offset, m.message.value.toString('utf8'));
+      var tempSharePrice = m.message.value.toString('utf8');
+      var tempShare = tempSharePrice;
+      console.log(tempShare);
+      var sharePrice = tempSharePrice.replace(/[^0-9]/g, '');
+      var share = tempShare.replace(/[^a-z,A-Z]/g, '');
+      io.emit('test event', { share: share, sharePrice: sharePrice });
     });
   };
-  console.log(consumer);
   return consumer.init().then(function () {
     // Subscribe partitons 0 and 1 in a topic: 
-    var v1 = consumer.subscribe('test', dataHandler);
-    // var arr = [];
-    // arr.push([v1);
-    console.log("val:" + v1);
-    // return arr;
-
+    consumer.subscribe('mysqlnew-iifl_realtimedb', dataHandler);
   });
 });
